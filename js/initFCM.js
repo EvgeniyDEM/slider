58bcfec220be181c6719ae316840faa5a04a191f
EvgeniyDEM <57892771+EvgeniyDEM@users.noreply.github.com>
2022-06-18 18:53:36 +0300

    Update initFCM.js

--- js/initFCM.js
+++ js/initFCM.js
@@ -1,1 +1,22 @@
-import 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-messaging.js";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+    apiKey: "AIzaSyCERsi4T3yJytJddIFprKBRgiTks7Q3eXg",
+  authDomain: "pushnotification-225412.firebaseapp.com",
+  databaseURL: "https://pushnotification-225412.firebaseio.com",
+  projectId: "pushnotification-225412",
+  storageBucket: "pushnotification-225412.appspot.com",
+  messagingSenderId: "234440847732",
+  appId: "1:234440847732:web:bf1f511ac862e69672fc21"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = getMessaging(app);
+
